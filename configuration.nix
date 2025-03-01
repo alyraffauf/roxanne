0b06b94819e998b49cbc7113cab2cbd61e755e73
Aly Raffauf <aly@raffauflabs.com>
2025-02-28 23:24:50 -0500

    cfg/services: add restic backups for uptime-kuma

--- configuration.nix
+++ configuration.nix
@@ -131,6 +131,44 @@
       settings.PasswordAuthentication = false;
     };
 
+    restic.backups = let
+      defaults = {
+        inhibitsSleep = true;
+        initialize = true;
+        passwordFile = config.age.secrets.restic-passwd.path;
+
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 4"
+          "--keep-monthly 6"
+          "--compression max"
+        ];
+
+        rcloneConfigFile = config.age.secrets.rclone-b2.path;
+
+        timerConfig = {
+          OnCalendar = "daily";
+          Persistent = true;
+          RandomizedDelaySec = "1h";
+        };
+      };
+    in {
+      uptime-kuma =
+        defaults
+        // {
+          backupCleanupCommand = ''
+            ${pkgs.systemd}/bin/systemctl start uptime-kuma
+          '';
+
+          backupPrepareCommand = ''
+            ${pkgs.systemd}/bin/systemctl stop uptime-kuma
+          '';
+
+          paths = ["/var/lib/uptime-kuma"];
+          repository = "rclone:b2:aly-backups/${config.networking.hostName}/uptime-kuma";
+        };
+    };
+
     tailscale = {
       enable = true;
       authKeyFile = config.age.secrets.tailscaleAuthKey.path;
