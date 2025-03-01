8a8da908e7b872871a93ef916452236373fd7887
Aly Raffauf <aly@raffauflabs.com>
2025-02-28 20:53:11 -0500

    cfg/programs: enable direnv

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,12 @@
   programs = {
     dconf.enable = true; # Needed for home-manager
 
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      silent = true;
+    };
+
     gnupg.agent = {
       enable = true;
       enableSSHSupport = true;
